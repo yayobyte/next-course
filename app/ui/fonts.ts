e72efef38146368b599e379d0372752b8d5992a7
yayobyte <yayobyte@gmail.com>
2024-05-26 18:13:16 +0400

    fetch data

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Roboto, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'], weight: '400'  });
+export const roboto = Roboto({ subsets: ['latin'], weight: '400'})
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400'})
